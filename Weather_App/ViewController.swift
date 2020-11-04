6123f891704483a2fadfc5313bfbb653cfb04d9d
SyedMuneebUrRehman94 <muneeb.aimviz@gmail.com>
2020-11-04 14:44:58 +0500

    Ui completed

---
+++ Weather_App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Weather_App
+//
+//  Created by Syed Muneeb Ur Rehman on 27/10/2020.
+//  Copyright © 2020 Syed Muneeb Ur Rehman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
